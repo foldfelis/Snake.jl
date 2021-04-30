912598fac3d2fcd1c8caf260725d9531efd8c3b3
JingYu Ning <foldfelis@gmail.com>
2021-04-30 23:03:01 +0800

    Move code from Terming

--- src/Snake.jl
+++ src/Snake.jl
@@ -1,5 +1,35 @@
 module Snake
 
-# Write your package code here.
+    using Terming
+    const T = Terming
+
+    export main
+
+    include("model.jl")
+    include("view.jl")
+    include("control.jl")
+
+    function App(term_size=(30, 80))
+        # event queue
+        listener = InputListener()
+        event_queue = listener.pipeline[end]
+
+        # model
+        ph, pw = term_size
+        snake_model = SnakeModel(ph, pw, event_queue)
+
+        # view
+        snake_view = SnakeView(snake_model)
+        form_view = FormView(term_size, [snake_view])
+
+        app = App(term_size, listener, snake_model, form_view)
+
+        return app
+    end
+
+    function main()
+        app = App()
+        run(app)
+    end
 
 end
